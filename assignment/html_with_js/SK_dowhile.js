896cb4eb9b2f8f14662e30686d23c5812b11ed58
Pavan M R <pavanmradder@gmail.com>
2026-05-14 21:37:36 +0530

    Implement login functionality with attempts limit

--- assignment/html_with_js/SK_dowhile.js
+++ assignment/html_with_js/SK_dowhile.js
@@ -1,1 +1,33 @@
-
+let btn = document.getElementById("loginBtn");
+let passwordInput = document.getElementById("password");
+let statusText = document.getElementById("status");
+let attemptsText = document.getElementById("attempts");
+let attempts = 3;
+btn.addEventListener("click", function(){
+    do{
+        let password = passwordInput.value;
+        if(password === "@123"){
+            statusText.textContent ="✅ Login Successful!";
+            statusText.style.color = "green";
+            btn.disabled = true;
+            passwordInput.disabled = true;
+            break;
+        }
+        else{
+            attempts--;
+            attemptsText.textContent =`Attempts Left : ${attempts}`;
+            if(attempts > 0){
+                statusText.textContent ="❌ Wrong Password!";
+                statusText.style.color = "red";
+                passwordInput.value = "";
+            }
+            else{
+                statusText.textContent ="🚫 Login Failed!";
+                statusText.style.color = "red";
+                btn.disabled = true;
+                passwordInput.disabled = true;
+            }
+            break;
+        }
+    }while(attempts > 0);
+});
